9c2ee524dc8e47302c5d1d2749ffa52c50b3befd
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-07-07 07:19:35 -0700

    added delete task function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,4 @@
 import { ADD, REMOVE } from '../constants/actionType'
 
-import { v4 as uuidv4 } from 'uuid';
-
 export const add = (task) => ({type: ADD, payload:task});
 export const remove = (id) => ({type: REMOVE, payload:id});
